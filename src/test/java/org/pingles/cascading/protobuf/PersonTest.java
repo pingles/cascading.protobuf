bf69ad52a9502c360bad3d50e9ac2f8c61c12b5a
Paul Ingles <paul@oobaloo.co.uk>
2012-01-27 08:48:20 +0000

    import test annotation

--- src/test/java/org/pingles/cascading/protobuf/PersonTest.java
+++ src/test/java/org/pingles/cascading/protobuf/PersonTest.java
@@ -1,9 +1,11 @@
 package org.pingles.cascading.protobuf;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PersonTest {
-    @org.junit.Test
+    @Test
     public void shouldConstructPerson() {
         Messages.Person.Builder builder = Messages.Person.newBuilder();
         builder.setEmail("test@pingles.org").setId(1).setName("Paul");
