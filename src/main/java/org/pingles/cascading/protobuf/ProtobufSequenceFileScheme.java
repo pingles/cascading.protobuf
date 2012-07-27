6076f29f66bdda000dbed80fea94dc4f62b64817
Paul Lam <paul.lam@forward.co.uk>
2012-07-27 18:01:52 +0100

    patches for upgrading

--- src/main/java/org/pingles/cascading/protobuf/ProtobufSequenceFileScheme.java
+++ src/main/java/org/pingles/cascading/protobuf/ProtobufSequenceFileScheme.java
@@ -24,154 +24,172 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProtobufSequenceFileScheme 
-		extends Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]> {
+public class ProtobufSequenceFileScheme extends
+		Scheme<JobConf, RecordReader, OutputCollector, Object[], Object[]> {
 
 	private static final long serialVersionUID = 1L;
-	
-	private static final Logger LOGGER = Logger.getLogger(ProtobufSequenceFileScheme.class);
-    private Descriptors.Descriptor descriptor;
-    private String messageClassName;
 
-    public ProtobufSequenceFileScheme(Class messageClass, Fields sourceFields) {
-        super(sourceFields);
-        this.messageClassName = messageClass.getName();
-    }
+	private static final Logger LOGGER = Logger
+			.getLogger(ProtobufSequenceFileScheme.class);
+	private Descriptors.Descriptor descriptor;
+	private String messageClassName;
 
+	public ProtobufSequenceFileScheme(Class messageClass, Fields sourceFields) {
+		super(sourceFields);
+		this.messageClassName = messageClass.getName();
+	}
+
+	private boolean sourceReadInput(
+			SourceCall<Object[], RecordReader> sourceCall) throws IOException 
+	{
+		Object[] context = sourceCall.getContext();
+		return sourceCall.getInput().next(context[0], context[1]);
+	}
 
 	@Override
-	public boolean source(FlowProcess<JobConf> arg0, SourceCall<Object[], RecordReader> sourceCall) throws IOException {
-        Fields sourceFields = getSourceFields();
-        Object key = sourceCall.getContext()[ 0 ];
-        Object val = sourceCall.getContext()[ 1 ];
-        boolean result = sourceCall.getInput().next( key, val );
-        
-        if( !result )
-            return false;
-        
-        Tuple tuple =  sourceCall.getIncomingEntry().getTuple();
-        tuple.clear();
-        byte[] valueBytes = toBytes((BytesWritable) val);
+	public boolean source(FlowProcess<JobConf> arg0,
+			SourceCall<Object[], RecordReader> sourceCall) throws IOException {
+		// Fields sourceFields = getSourceFields();
+		Object key = sourceCall.getContext()[0];
+		Object val = sourceCall.getContext()[1];
 
-        try {
-            DynamicMessage message = parseMessage(getMessageDescriptor(), valueBytes);
-            List<String> fieldNames = getFieldNames();
+		if (!sourceReadInput(sourceCall))
+			return false;
 
-            for (String fieldName : fieldNames) {
-                Descriptors.FieldDescriptor fieldDescriptor = getFieldDescriptor(getMessageDescriptor(), fieldName);
-                
-                if (!fieldDescriptor.isRepeated()) {
-                    Object fieldValue = message.getField(fieldDescriptor);
-                    tuple.add(convertToTupleObject(fieldValue));
-                } else {
-                    Tuple t = new Tuple();
-                    for (int i1 = 0; i1 < message.getRepeatedFieldCount(fieldDescriptor); i1++) {
-                        Object fieldValue = message.getRepeatedField(fieldDescriptor, i1);
-                        t.add(convertToTupleObject(fieldValue));
-                    }
-                    tuple.addAll(t);
-                }
+		Tuple tuple = sourceCall.getIncomingEntry().getTuple();
+		tuple.clear();
+		byte[] valueBytes = toBytes((BytesWritable) val);
 
-            }
-        } catch (InvalidProtocolBufferException e) {
-            throw new RuntimeException(e);
-        }
+		try {
+			DynamicMessage message = parseMessage(getMessageDescriptor(),
+					valueBytes);
+			List<String> fieldNames = getFieldNames();
 
-        return true;
-    }
+			for (String fieldName : fieldNames) {
+				Descriptors.FieldDescriptor fieldDescriptor = getFieldDescriptor(
+						getMessageDescriptor(), fieldName);
 
-    private List<String> getFieldNames() {
-        List<String> fieldNames = new ArrayList<String>();
-        if (getSourceFields().size() == 0) {
-            List<Descriptors.FieldDescriptor> fields = getMessageDescriptor().getFields();
-            for (Descriptors.FieldDescriptor field : fields) {
-                fieldNames.add(field.getName());
-            }
-        } else {
-            for (int i = 0; i < getSourceFields().size(); i++) {
-                fieldNames.add(getSourceFields().get(i).toString());
-            }
-        }
-        return fieldNames;
-    }
+				if (!fieldDescriptor.isRepeated()) {
+					Object fieldValue = message.getField(fieldDescriptor);
+					tuple.add(convertToTupleObject(fieldValue));
+				} else {
+					Tuple t = new Tuple();
+					for (int i1 = 0; i1 < message
+							.getRepeatedFieldCount(fieldDescriptor); i1++) {
+						Object fieldValue = message.getRepeatedField(
+								fieldDescriptor, i1);
+						t.add(convertToTupleObject(fieldValue));
+					}
+					tuple.add(t);
+				}
 
-    private Object convertToTupleObject(Object fieldValue) {
-        if (fieldValue instanceof  DynamicMessage) {
-            DynamicMessage msg = (DynamicMessage)fieldValue;
-            List<Descriptors.FieldDescriptor> fields = msg.getDescriptorForType().getFields();
-            Tuple t = new Tuple();
-            for (Descriptors.FieldDescriptor field : fields) {
-                Object value = msg.getField(field);
-                t.add(value);
-            }
-            return t;
-        } else {
-            return fieldValue;
-        }
-    }
+			}
+		} catch (InvalidProtocolBufferException e) {
+			throw new RuntimeException(e);
+		}
 
-    private Descriptors.FieldDescriptor getFieldDescriptor(Descriptors.Descriptor messageDescriptor, String fieldName) {
-        List<Descriptors.FieldDescriptor> fields = messageDescriptor.getFields();
-        for (Descriptors.FieldDescriptor field : fields) {
-            if (field.getName().equals(fieldName)) {
-                return field;
-            }
-        }
-        throw new RuntimeException("No such field " + fieldName + " on message");
-    }
+		return true;
+	}
 
-    private DynamicMessage parseMessage(Descriptors.Descriptor messageDescriptor, byte[] valueBytes) throws InvalidProtocolBufferException {
-        DynamicMessage.Builder builder = DynamicMessage.newBuilder(messageDescriptor).mergeFrom(valueBytes);
-        DynamicMessage message = builder.build();
-        return message;
-    }
+	private List<String> getFieldNames() {
+		List<String> fieldNames = new ArrayList<String>();
+		if (getSourceFields().size() == 0) {
+			List<Descriptors.FieldDescriptor> fields = getMessageDescriptor()
+					.getFields();
+			for (Descriptors.FieldDescriptor field : fields) {
+				fieldNames.add(field.getName());
+			}
+		} else {
+			for (int i = 0; i < getSourceFields().size(); i++) {
+				fieldNames.add(getSourceFields().get(i).toString());
+			}
+		}
+		return fieldNames;
+	}
 
-    private Descriptors.Descriptor getMessageDescriptor() {
-        if (descriptor == null) {
-            try {
-                Class messageClass = Class.forName(messageClassName);
-                Method descriptorMethod = messageClass.getMethod("getDescriptor");
-                descriptor = (Descriptors.Descriptor) descriptorMethod.invoke(new Object[]{});
-            } catch (IllegalAccessException e) {
-                throw new RuntimeException(e);
-            } catch (InvocationTargetException e) {
-                throw new RuntimeException(e);
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return descriptor;
-    }
+	private Object convertToTupleObject(Object fieldValue) {
+		if (fieldValue instanceof DynamicMessage) {
+			DynamicMessage msg = (DynamicMessage) fieldValue;
+			List<Descriptors.FieldDescriptor> fields = msg
+					.getDescriptorForType().getFields();
+			Tuple t = new Tuple();
+			for (Descriptors.FieldDescriptor field : fields) {
+				Object value = msg.getField(field);
+				t.add(value);
+			}
+			return t;
+		} else {
+			return fieldValue;
+		}
+	}
 
-    private byte[] toBytes(BytesWritable val) {
-        byte[] bytes = val.getBytes();
-        byte[] valueBytes = new byte[val.getLength()];
-        System.arraycopy(bytes, 0, valueBytes, 0, val.getLength());
-        return valueBytes;
-    }
+	private Descriptors.FieldDescriptor getFieldDescriptor(
+			Descriptors.Descriptor messageDescriptor, String fieldName) {
+		List<Descriptors.FieldDescriptor> fields = messageDescriptor
+				.getFields();
+		for (Descriptors.FieldDescriptor field : fields) {
+			if (field.getName().equals(fieldName)) {
+				return field;
+			}
+		}
+		throw new RuntimeException("No such field " + fieldName + " on message");
+	}
+
+	private DynamicMessage parseMessage(
+			Descriptors.Descriptor messageDescriptor, byte[] valueBytes)
+			throws InvalidProtocolBufferException {
+		DynamicMessage.Builder builder = DynamicMessage.newBuilder(
+				messageDescriptor).mergeFrom(valueBytes);
+		DynamicMessage message = builder.build();
+		return message;
+	}
+
+	private Descriptors.Descriptor getMessageDescriptor() {
+		if (descriptor == null) {
+			try {
+				Class<?> messageClass = Class.forName(messageClassName);
+				Method descriptorMethod = messageClass
+						.getMethod("getDescriptor");
+				descriptor = (Descriptors.Descriptor) descriptorMethod
+						.invoke(new Object[] {});
+			} catch (IllegalAccessException e) {
+				throw new RuntimeException(e);
+			} catch (InvocationTargetException e) {
+				throw new RuntimeException(e);
+			} catch (ClassNotFoundException e) {
+				throw new RuntimeException(e);
+			} catch (NoSuchMethodException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return descriptor;
+	}
+
+	private byte[] toBytes(BytesWritable val) {
+		byte[] bytes = val.getBytes();
+		byte[] valueBytes = new byte[val.getLength()];
+		System.arraycopy(bytes, 0, valueBytes, 0, val.getLength());
+		return valueBytes;
+	}
 
 	@Override
 	public void sink(FlowProcess<JobConf> arg0,
 			SinkCall<Object[], OutputCollector> arg1) throws IOException {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void sinkConfInit(FlowProcess<JobConf> arg0,
 			Tap<JobConf, RecordReader, OutputCollector> arg1, JobConf arg2) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void sourceConfInit(FlowProcess<JobConf> arg0,
 			Tap<JobConf, RecordReader, OutputCollector> arg1, JobConf arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
